2da5a6f29c2810b484e59d759dce0745bef7c368
NmnemoniX <yzhakovng@rambler.ru>
2021-11-30 19:35:28 +0500

    first (no promise)

---
+++ number_game.js
@@ -0,0 +1,26 @@
+const  readline = require('readline')
+
+const input = readline.createInterface(process.stdin , process.stdout)
+
+
+console.log("/***************!начало игры!*************************/")
+console.log("/**********вам необходимо отгадать число!*************/")
+console.log("/******как только отгадаете , то игра окончится!******/")
+console.log("/******************введите первое число!**************/")
+
+input.on('line',(data)=>{
+
+      if (data==qest_number){
+        console.log("/*****************************************************/")
+        console.log("/********************ПОБЕДА!**************************/")
+        console.log("/*****************************************************/")
+        process.exit(0);
+    }
+    else if (data>qest_number)
+    {
+        console.log("/меньше/")
+    }
+    else {
+        console.log("/больше/")
+    }
+})
